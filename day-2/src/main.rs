7a593d977883ed430c5035f09b4f81991483012a
Joshua Fryer <josh.s.fryer@gmail.com>
2024-01-11 18:51:32 -0500

    Part 1 Solution

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -1,15 +1,74 @@
-use std::{collections::HashMap, fs};
+use std::fs;
 
 // Structure to track the greatest number of each colour of cubes in a game
 // struct foobar
 
-struct GameMaximums {
+#[derive(Debug)]
+struct Game {
+    id: u32,
+    maxima: ColourCount,
+}
+
+#[derive(Debug)]
+struct ColourCount {
     red: u32,
     green: u32,
     blue: u32,
 }
 
-// Associate
+fn get_count_from_elem(elem: &str) -> u32 {
+    let digits = elem.matches(char::is_numeric);
+    let mut sum = 0;
+
+    for (place_val, digit) in digits.rev().enumerate() {
+        let base = digit.parse::<u32>().unwrap();
+        let exponent = u32::try_from(place_val).unwrap();
+        let scalar = 10_u32.pow(exponent);
+        let full_number = base * scalar;
+        sum += full_number;
+    }
+    return sum;
+}
+
+fn parse_draws(draws: Vec<&str>) -> ColourCount {
+    let mut maxima = ColourCount {
+        red: 0,
+        green: 0,
+        blue: 0,
+    };
+    for draw in draws {
+        // Split each draw in to constituent colours.
+        let draw_elements = draw.split(',');
+        let mut draw_counts = ColourCount {
+            red: 0,
+            green: 0,
+            blue: 0,
+        };
+        // Get the red, green, blue counts.
+        for elem in draw_elements {
+            if elem.contains("red") {
+                draw_counts.red = get_count_from_elem(elem);
+            } else if elem.contains("green") {
+                draw_counts.green = get_count_from_elem(elem);
+            } else if elem.contains("blue") {
+                draw_counts.blue = get_count_from_elem(elem);
+            }
+        }
+
+        // If this draw showed more of a colour than we've seen so far, update
+        // the maximums.
+        if draw_counts.red > maxima.red {
+            maxima.red = draw_counts.red;
+        }
+        if draw_counts.green > maxima.green {
+            maxima.green = draw_counts.green;
+        }
+        if draw_counts.blue > maxima.blue {
+            maxima.blue = draw_counts.blue;
+        }
+    }
+    return maxima;
+}
 
 fn main() {
     let file_path = "./src/input.txt";
@@ -20,58 +79,40 @@ fn main() {
     const MAX_BLUE: u32 = 14;
 
     let contents: String = fs::read_to_string(file_path).expect("Unable to read input file");
-
+    let mut games_list: Vec<Game> = vec![];
     // Each line has several draws, each delineated by a semicolon
     // Each is also identified by an ID, "Game N: {...}"
-    let mut games_map: HashMap<&str, GameMaximums> = HashMap::new();
     for line in contents.lines() {
+        // Split the line into Game ID and the list of draws.
         let mut split_line = line.split(':');
         let game_id: &str = split_line.next().unwrap().split(' ').last().unwrap();
-        let draws = split_line.next().unwrap().split(';');
-
-        let mut maximums = GameMaximums {
-            red: 0,
-            green: 0,
-            blue: 0,
+        let game_id: u32 = game_id.parse().unwrap();
+        let draws: Vec<&str> = split_line.next().unwrap().split(';').collect();
+        let game_maxima = parse_draws(draws);
+        let curr_game = Game {
+            id: game_id,
+            maxima: game_maxima,
         };
-        for draw in draws {
-            // red/green/blue values are split by commas
-            let elems = draw.split(',');
-            for elem in elems {
-                if draw.contains("red") {
-                    let red_count = draw
-                        .matches(char::is_numeric)
-                        .next()
-                        .unwrap()
-                        .parse::<u32>()
-                        .unwrap();
-                    if red_count > maximums.red {
-                        maximums.red = red_count;
-                    }
-                } else if draw.contains("green") {
-                    let green_count = draw
-                        .matches(char::is_numeric)
-                        .next()
-                        .unwrap()
-                        .parse::<u32>()
-                        .unwrap();
-                    if green_count > maximums.green {
-                        maximums.green = green_count;
-                    }
-                } else if draw.contains("blue") {
-                    let blue_count = draw
-                        .matches(char::is_numeric)
-                        .next()
-                        .unwrap()
-                        .parse::<u32>()
-                        .unwrap();
-                    if blue_count > maximums.blue {
-                        maximums.blue = blue_count;
-                    }
-                }
-            }
-        }
-        println!(".");
-        games_map.insert(game_id, maximums);
+        games_list.push(curr_game);
     }
+
+    let mut valid_games: Vec<Game> = vec![];
+    for game in games_list {
+        // Check whether the game is valid; i.e. whether none of its draws had
+        // any colour count exceeding the MAX_*** limits.
+        if game.maxima.red <= MAX_RED
+            && game.maxima.green <= MAX_GREEN
+            && game.maxima.blue <= MAX_BLUE
+        {
+            valid_games.push(game);
+        }
+    }
+    println!("{:?}", valid_games);
+    // Sum the IDs of valid games
+    let mut sum_of_ids = 0;
+    for game in valid_games {
+        sum_of_ids += game.id;
+    }
+
+    println!("The sum of all valid game IDs is {sum_of_ids}");
 }
